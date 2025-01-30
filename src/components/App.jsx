0d5780234547ada80f996b49edf16cf21957fbf4
Frandy Slueue <frandy4ever@gmail.com>
2025-01-29 22:49:32 -0600

    Added VolumeControls with dynamic left-fill effect to indicate volume being increase or decrease

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,13 +3,15 @@ import Footer from "./Footer";
 import MusicPlayer from "./MusicPlayer";
 import PlayControls from "./PlayControls";
 import SongTitle from "./SongTitle";
+import VolumeControls from "./VolumeControls";
 
 export default function App() {
   return (
     <div className="flex items-center justify-center min-h-screen bg-slate-900 text-slate-200">
       <CoverArt />
       <SongTitle title="Blinding Lights" artist="The Weeknd" />
-      <PlayControls/>
+      <PlayControls />
+      <VolumeControls/>
       <MusicPlayer />
       <Footer/>
     </div>
