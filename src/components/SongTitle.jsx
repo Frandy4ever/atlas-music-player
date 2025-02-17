9c26ebf4cf231fef76e02849bb8b57df3def251d
Frandy Slueue <frandy4ever@gmail.com>
2025-02-17 06:20:44 -0600

    Removed all files/folders for major update

--- src/components/SongTitle.jsx
+++
@@ -1,9 +0,0 @@
-export default function SongTitle({ title = "Unknown Song", artist = "Unknown Artist" }) {
-    return (
-      <div className="text-center">
-        <h2 className="text-lg md:text-xl font-semibold text-white font-bold">{title}</h2>
-        <p className="text-sm md:text-base text-blue-300">{artist}</p>
-      </div>
-    );
-  }
-  
